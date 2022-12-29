79cdcf7e856f1b33a99a599023907b22b7f761af
rmcats <rmcats@gmail.com>
2022-12-29 08:17:05 +0530

    all data commit

---
+++ my.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a=5,b=2;
+    cout<<a/b;
+    
+}
---
+++ myw.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    switch(97){
+        case 1: cout<<"one";
+        case 'a': cout<<"two";
+        case 3: cout<<"three";
+        case 4: cout<<"four";
+        case 5: cout<<"five";
+    }
+}
